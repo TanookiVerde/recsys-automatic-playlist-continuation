e60251dcd3a5508a58c71d729a54e7ff5a7a76a5
Pedro Nascimento <pedrovitor.marques@gmail.com>
2023-12-05 21:01:10 -0300

    Melhorias Page Rank

--- implementacao/models/personalized_pagerank.py
+++ implementacao/models/personalized_pagerank.py
@@ -6,12 +6,12 @@ import pickle
 
 class PersonalizedPageRank():
     
-    def __init__(self, scipy_adj_matrix = None, dgl_network = None, walk_depth=100, n_rounds = 1, rank_limit=500):
+    def __init__(self, scipy_adj_matrix = None, dgl_network = None, visit_target = 1, rank_limit=500, reset_prob=0.0):
         assert scipy_adj_matrix != None or dgl_network != None, "Input DGL Network or Scipy Adjacency Matrix"
 
-        self.walk_depth     = walk_depth
-        self.n_rounds        = n_rounds
+        self.visit_target   = visit_target
         self.rank_limit     = rank_limit
+        self.reset_prob     = reset_prob
 
         if scipy_adj_matrix != None:
             self.from_scipy(scipy_adj_matrix)
@@ -34,7 +34,7 @@ class PersonalizedPageRank():
             weight = scipy_adj_matrix[init_nodes[i], final_nodes[i]]
             weights.append( weight )
 
-        self.network.edata['weights'] = torch.tensor(weights)
+        self.network.edata['weights'] = torch.tensor(weights, dtype=float)
         
         pass
     
@@ -50,27 +50,29 @@ class PersonalizedPageRank():
 
         
     def predict_one(self, query):
-        visited_nodes = []
-        for i in range(self.n_rounds):
-            paths, _ = dgl.sampling.random_walk(
-                g       = self.network, 
-                nodes   = query, 
-                length  = self.walk_depth, 
-                prob    = 'weights'
-            )
-            visited_nodes_in_round = np.concatenate(paths.numpy()).tolist()
-            visited_nodes.extend(visited_nodes_in_round)
+        from math import ceil
 
         rank = {}
-        for node in visited_nodes:
-            # Remove nodes from Query and -1 (a truncation indicator of DGL)
-            if node == -1 or node in query:
-                continue
+        visits_per_node = ceil( self.visit_target / len(query) )
 
-            if node in rank.keys():
-                rank[node] += 1
-            else:
-                rank[node] = 1
+        paths, _ = dgl.sampling.random_walk(
+            g               = self.network, 
+            nodes           = query, 
+            length          = visits_per_node, 
+            prob            = 'weights',
+            restart_prob    = self.reset_prob
+        )
+
+        for path in paths:
+            for node in path:
+                # Remove nodes from Query and -1 (a truncation indicator of DGL)
+                if node == -1 or node in query:
+                    continue
+
+                if node in rank.keys():
+                    rank[node] += 1
+                else:
+                    rank[node] = 1
 
         rank = sorted(rank.items(), key=lambda x:x[1], reverse=True)
         ranked_nodes    = [x for x, _ in rank]
